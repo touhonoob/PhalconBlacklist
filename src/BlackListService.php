df7fd57c7147b310da2c35ce42e27a3e87a427e4
touhonoob <touhonoob@gmail.com>
2015-05-21 00:48:01 +0800

    DI => Di

--- src/BlackListService.php
+++ src/BlackListService.php
@@ -6,7 +6,7 @@ namespace PhalconBlacklist;
  * @author Peter Chung <touhonoob@gmail.com>
  * @date Mar 5, 2015
  */
-class BlackListService extends \Phalcon\DI\Injectable
+class BlackListService extends \Phalcon\Di\Injectable
 {
 
     public function __construct()
